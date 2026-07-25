bfe54650c1fa499332223bf1fce26114f1ef114a
Thiago Trotta <trottathiago1@gmail.com>
2026-07-25 06:44:33 -0300

    feat: wire post:slug route with component input binding and SSR

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,8 @@ export const routes: Routes = [
         path: '',
         loadComponent: () => import('./pages/home/home').then(m => m.Home)
     },
+    {
+        path: 'post/:slug',
+        loadComponent: () => import('./pages/post-detail/post-detail').then(m => m.PostDetail)
+    },
 ];
